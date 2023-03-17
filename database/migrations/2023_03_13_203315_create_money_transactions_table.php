75459d9ef87f97e69a81731eb255c991184b0738
nikolay mikov <nikolay.mikov1@gmail.com>
2023-03-17 22:37:38 +0200

    add: Defining the database migration file for the money_transactions table

---
+++ database/migrations/2023_03_13_203315_create_money_transactions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('money_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('transaction_amount', 9)->default(0);
+            $table->json('transaction_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('money_transactions');
+    }
+};
